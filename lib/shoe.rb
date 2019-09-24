13fff4e2865a011700e5a72920b5994c4f140ce2
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-24 22:55:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+def Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+end
